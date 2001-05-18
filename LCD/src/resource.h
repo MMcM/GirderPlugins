da9e9c2cc51baeb04cd3c82d2c4584e06cdd7322
Mike McMahon <mmcm@sourceforge.net>
2001-05-18 18:16:24 +0000

    Import release 1.5

---
+++ LCD/src/resource.h
@@ -0,0 +1,35 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Developer Studio generated include file.
+// Used by ui.rc
+//
+#define PLUGINICON                      101
+#define IDD_DIALOG                      102
+#define IDC_APPLY                       1000
+#define IDC_TYPEL                       1001
+#define IDC_TYPE                        1002
+#define IDC_VALUEL                      1003
+#define IDC_VALSTR                      1004
+#define IDC_VALINT                      1005
+#define IDC_VAL_SPIN                    1006
+#define IDC_ROWL                        1007
+#define IDC_ROW                         1008
+#define IDC_ROW_SPIN                    1009
+#define IDC_COLL                        1010
+#define IDC_COL                         1011
+#define IDC_COL_SPIN                    1012
+#define IDC_USE_REST                    1013
+#define IDC_USE_WIDTH                   1014
+#define IDC_WIDTH                       1015
+#define IDC_WIDTH_SPIN                  1016
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NO_MFC                     1
+#define _APS_NEXT_RESOURCE_VALUE        103
+#define _APS_NEXT_COMMAND_VALUE         40001
+#define _APS_NEXT_CONTROL_VALUE         1017
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
