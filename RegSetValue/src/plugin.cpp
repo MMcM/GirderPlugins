c11dcacf261e97d5372cf3cab5eebe7cdf12a84d
Mike McMahon <mmcm@sourceforge.net>
2002-05-18 15:58:30 +0000

    Import release 1.3

---
+++ RegSetValue/src/plugin.cpp
@@ -0,0 +1,213 @@
+/***************************************************************************************/
+/*                                                                                     */
+/*  Girder 3.1 Plugin                                                                  */
+/*  User interface                                                                     */
+/*                                                                                     */
+/*  Copyright 2000 (c) Ron Bessems                                                     */
+/*  GNU General Public License                                                         */
+/*                                                                                     */
+/*                                                                                     */
+/***************************************************************************************/
+
+#define WIN32_LEAN_AND_MEAN
+#define GIRDER_CPP
+
+#include <windows.h>
+#include "plugin.h"
+#include "ui.h"
+#include <stdio.h>
+
+
+
+
+
+extern "C" int WINAPI  gir_open(int gir_major_ver, int gir_minor_ver, int gir_micro_ver, p_functions p)
+{
+	
+  p_functions h = (p_functions )	p;
+  if ( h->size != sizeof( s_functions ) )
+    {
+      return GIR_FALSE;
+    }
+		
+  memcpy((void *)&SF, p, sizeof ( s_functions ));
+
+
+  return GIR_TRUE;
+
+}
+
+
+
+extern "C" int WINAPI gir_close()
+{
+
+   Close_Config();
+
+   return GIR_TRUE;
+}
+
+
+
+
+PCHAR DecodeKeyValue(PCHAR value, PHKEY phkey)
+{
+  char buf[1024];
+  strncpy(buf, value, sizeof(buf));
+  
+  LPSTR pkey, pval;
+  pkey = strchr(buf, '\\');
+  if (NULL != pkey)
+    *pkey++ = '\0';
+  else
+    pkey = buf + strlen(buf);
+
+  HKEY hkey;
+  if (!strcmp(buf, "HKCU") || !strcmp(buf, "HKEY_CURRENT_USER"))
+    hkey = HKEY_CURRENT_USER;
+  else if (!strcmp(buf, "HKLM") || !strcmp(buf, "HKEY_LOCAL_MACHINE"))
+    hkey = HKEY_LOCAL_MACHINE;
+  else {
+    sprintf(buf, "Unknown register root: %s", value);
+    MessageBox(0, buf, "Error", MB_ICONERROR);
+    return NULL;
+  }
+
+  pval = strrchr(pkey, '\\');
+  if (NULL != pval)
+    *pval++ = '\0';
+  else
+    pval = pkey + strlen(pkey);
+  if (!strcmp(pval, "@"))       // Allow RegEdit convention for unnamed value.
+    pval++;
+
+  if (ERROR_SUCCESS != RegOpenKey(hkey, pkey, &hkey)) {
+    sprintf(buf, "Key does not exist: %s", value);
+    MessageBox(0, buf, "Error", MB_ICONERROR);
+    return NULL;
+  }
+  
+  *phkey = hkey;
+  return value + (pval - buf);
+}
+
+
+
+extern "C" int WINAPI gir_event(p_command command, char *eventstring, void * payload, int len, char * status,  int statuslen)
+{
+  HKEY hkey;
+
+  strncpy(status, "Registry update error.", statuslen);
+
+  EnterCriticalSection(&command->critical_section);
+
+  PCHAR pval = DecodeKeyValue(command->svalue1, &hkey);
+  
+
+  if (NULL == pval)
+  {
+    LeaveCriticalSection(&command->critical_section);
+    return retContinue;
+  }
+
+  LONG rc;
+  if (command->bvalue1) {
+    
+    DWORD value = command->lvalue1;
+    
+    rc = RegSetValueEx(hkey, pval, 0, REG_DWORD, (LPBYTE)&value, sizeof(value));
+  }
+  else {
+    char buf[1024];
+    SF.parse_reg_string(command->svalue2, buf, sizeof(buf));
+    rc = RegSetValueEx(hkey, pval, 0, REG_SZ, (LPBYTE)buf, strlen(buf));
+  }
+  
+  LeaveCriticalSection(&command->critical_section);
+  
+  RegCloseKey(hkey);
+
+  if (ERROR_SUCCESS != rc) {
+    LeaveCriticalSection(&command->critical_section);
+    MessageBox(0, "Error setting registry value", "Error", MB_ICONERROR);
+    return retContinue;
+  }
+
+  strncpy(status, "Registry updated.", statuslen);
+
+  return retContinue;
+}
+
+
+extern "C" void WINAPI gir_command_changed(p_command command)
+{
+
+  if ( command == NULL ) 
+    {
+      Enable_Config(FALSE);
+    }
+  else
+    {
+      Enable_Config(TRUE);
+      CurCommand = command;	
+
+
+      Update_Config();
+    }
+
+}
+
+extern "C" void WINAPI gir_command_gui()
+{
+
+  Show_Config();
+  
+}
+
+
+extern "C" int WINAPI gir_requested_api(int maxapi)
+{
+  return 1;
+}
+
+extern "C" int WINAPI gir_devicenum()
+{
+  return PLUGINNUM;
+}
+
+extern "C" void WINAPI gir_description(PCHAR Buffer, BYTE Length)
+{
+  strncpy(Buffer, "Change Windows registry value.  Written Mike McMahon (MMcM).", Length);
+}
+
+extern "C" void WINAPI gir_name(PCHAR Buffer, BYTE Length)
+{
+  strncpy(Buffer, PLUGINNAME, Length);
+}
+
+extern "C" void WINAPI gir_version(PCHAR Buffer, BYTE Length)
+{
+  strncpy(Buffer, PLUGINVERSION, Length);
+}
+
+
+
+
+/* Called by windows */
+BOOL WINAPI DllMain( HANDLE hModule, DWORD fdwreason,  LPVOID lpReserved )
+{
+  UNREFERENCED_PARAMETER(lpReserved);
+  
+  switch(fdwreason) {
+  case DLL_PROCESS_ATTACH:
+    hInstance=HINSTANCE(hModule);
+    break;
+  case DLL_THREAD_ATTACH:
+    break;
+  case DLL_THREAD_DETACH:
+    break;
+  case DLL_PROCESS_DETACH:
+    break;
+  }
+  return TRUE;
+}
---
+++ RegSetValue/src/plugin.h
@@ -0,0 +1,34 @@
+/***************************************************************************************/
+/*                                                                                     */
+/*  Girder 3.1 Plugin                                                                  */
+/*  User interface                                                                     */
+/*                                                                                     */
+/*  Copyright 2000 (c) Ron Bessems                                                     */
+/*  GNU General Public License                                                         */
+/*                                                                                     */
+/*                                                                                     */
+/***************************************************************************************/
+
+#define PLUGINNUM 117
+#define PLUGINNAME "RegSetValue"
+#define PLUGINVERSION "1.3"
+
+#include <girder.h>
+
+
+#ifdef GIRDER_CPP
+	p_command				CurCommand;
+   s_functions          SF;
+	HINSTANCE				hInstance;
+#endif
+
+#ifndef GIRDER_CPP
+	extern p_command			 CurCommand;		
+   extern s_functions       SF;
+	extern HINSTANCE	   	hInstance;
+#endif
+
+
+/* Commmon routines */
+
+extern PCHAR DecodeKeyValue(PCHAR value, PHKEY phkey);
