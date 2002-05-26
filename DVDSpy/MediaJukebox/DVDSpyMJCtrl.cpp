4d169518c0b261553741efc9c0ad16d0a1f38b69
Mike McMahon <mmcm@sourceforge.net>
2002-05-26 14:48:10 +0000

    Import release 1.26

---
+++ DVDSpy/MediaJukebox/DVDSpyMJCtrl.cpp
@@ -0,0 +1,125 @@
+// DVDSpyMJCtrl.cpp : Implementation of CDVDSpyMJCtrl
+
+#include "stdafx.h"
+#include "DVDSpyMJ.h"
+#include "DVDSpyMJCtrl.h"
+#include "DVDSpyMJEvent.h"
+
+HWND g_hwndDVDSpy = NULL;
+
+BOOL CALLBACK FindMonitorWindow(HWND hwnd, LPARAM lparam)
+{
+  char cname[256];
+  GetClassName(hwnd, cname, sizeof(cname));
+  if (!strcmp(cname, "Girder DVDSpy Monitor Window")) {
+    *(HWND*)lparam = hwnd;
+    return FALSE;
+  }
+  return TRUE;                  // Keep trying.
+}
+
+/////////////////////////////////////////////////////////////////////////////
+// CDVDSpyMJCtrl
+
+
+CDVDSpyMJCtrl::CDVDSpyMJCtrl()
+{
+  m_bWindowOnly = TRUE;
+  CalcExtent(m_sizeExtent);
+
+  m_nRefreshInterval = 1000;
+}
+
+CDVDSpyMJCtrl::~CDVDSpyMJCtrl()
+{
+}
+
+STDMETHODIMP CDVDSpyMJCtrl::Init(LPDISPATCH pDisp)
+{
+  AFX_MANAGE_STATE(AfxGetStaticModuleState())
+
+  m_pMJ = pDisp;
+
+  char szRegistryPath[1024];
+  strcpy(szRegistryPath, REGISTRY_PATH_MJ_PLUGINS_INTERFACE);
+  strcat(szRegistryPath, "DVDSpy");
+  CRegKey reg;
+  if (ERROR_SUCCESS == reg.Create(HKEY_CURRENT_USER, szRegistryPath)) {
+    DWORD dwVal, dwType, dwSize;
+    dwSize = sizeof(dwVal);
+    if ((ERROR_SUCCESS == RegQueryValueEx(reg, "RefreshInterval", NULL,
+                                          &dwType, (LPBYTE)&dwVal, &dwSize)) &&
+        (REG_DWORD == dwType)) {
+      m_nRefreshInterval = dwVal;
+    }
+  }
+
+  CString sRefreshInterval;
+  sRefreshInterval.Format("%d", m_nRefreshInterval);
+  ::SetWindowText(GetDlgItem(IDC_INTERVAL), sRefreshInterval);
+
+  Reset();
+
+  return S_OK;
+}
+
+STDMETHODIMP CDVDSpyMJCtrl::get_RefreshInterval(short *pVal)
+{
+  AFX_MANAGE_STATE(AfxGetStaticModuleState())
+
+  *pVal = (short)m_nRefreshInterval;
+
+  return S_OK;
+}
+
+STDMETHODIMP CDVDSpyMJCtrl::put_RefreshInterval(short newVal)
+{
+  AFX_MANAGE_STATE(AfxGetStaticModuleState())
+
+  m_nRefreshInterval = newVal;
+
+  char szRegistryPath[1024];
+  strcpy(szRegistryPath, REGISTRY_PATH_MJ_PLUGINS_INTERFACE);
+  strcat(szRegistryPath, "DVDSpy");
+  CRegKey reg;
+  if (ERROR_SUCCESS == reg.Create(HKEY_CURRENT_USER, szRegistryPath)) {		
+    reg.SetValue(m_nRefreshInterval, "RefreshInterval");
+  }
+
+  return S_OK;
+}
+
+const UINT IDT_REFRESH = 1;
+
+STDMETHODIMP CDVDSpyMJCtrl::Reset()
+{
+  AFX_MANAGE_STATE(AfxGetStaticModuleState())
+
+  KillTimer(IDT_REFRESH);
+
+  EnumWindows(FindMonitorWindow, (LPARAM)&g_hwndDVDSpy);
+  if (NULL != g_hwndDVDSpy)
+    SetTimer(IDT_REFRESH, m_nRefreshInterval);
+
+  return S_OK;
+}
+
+LRESULT CDVDSpyMJCtrl::OnClickedReset(WORD wNotifyCode, WORD wID, HWND hWndCtl, BOOL& bHandled)
+{
+  char szRefreshInterval[32];
+  ::GetWindowText(GetDlgItem(IDC_INTERVAL), szRefreshInterval, sizeof(szRefreshInterval));
+
+  int nRefreshInterval = atoi(szRefreshInterval);
+  if (nRefreshInterval != m_nRefreshInterval)
+    put_RefreshInterval((short)nRefreshInterval);
+
+  Reset();
+  return 0;
+}
+
+LRESULT CDVDSpyMJCtrl::OnTimer(UINT uMsg, WPARAM wParam, LPARAM lParam, BOOL& bHandled)
+{
+  DVDSpyMJDoEvents(m_pMJ, g_hwndDVDSpy);
+
+  return TRUE;
+}
---
+++ DVDSpy/MediaJukebox/DVDSpyMJCtrl.h
@@ -0,0 +1,113 @@
+// DVDSpyMJCtrl.h : Declaration of the CDVDSpyMJCtrl
+
+#ifndef __DVDSPYMJCTRL_H_
+#define __DVDSPYMJCTRL_H_
+
+#include "resource.h"       // main symbols
+#include <atlctl.h>
+
+
+/////////////////////////////////////////////////////////////////////////////
+// CDVDSpyMJCtrl
+class ATL_NO_VTABLE CDVDSpyMJCtrl : 
+  public CComObjectRootEx<CComSingleThreadModel>,
+  public IDispatchImpl<IDVDSpyMJCtrl, &IID_IDVDSpyMJCtrl, &LIBID_DVDSPYMJLib>,
+  public CComCompositeControl<CDVDSpyMJCtrl>,
+  public IPersistStreamInitImpl<CDVDSpyMJCtrl>,
+  public IOleControlImpl<CDVDSpyMJCtrl>,
+  public IOleObjectImpl<CDVDSpyMJCtrl>,
+  public IOleInPlaceActiveObjectImpl<CDVDSpyMJCtrl>,
+  public IViewObjectExImpl<CDVDSpyMJCtrl>,
+  public IOleInPlaceObjectWindowlessImpl<CDVDSpyMJCtrl>,
+  public IPersistStorageImpl<CDVDSpyMJCtrl>,
+  public ISpecifyPropertyPagesImpl<CDVDSpyMJCtrl>,
+  public IQuickActivateImpl<CDVDSpyMJCtrl>,
+  public IDataObjectImpl<CDVDSpyMJCtrl>,
+  public IProvideClassInfo2Impl<&CLSID_DVDSpyMJCtrl, NULL, &LIBID_DVDSPYMJLib>,
+  public CComCoClass<CDVDSpyMJCtrl, &CLSID_DVDSpyMJCtrl>
+{
+public:
+  CDVDSpyMJCtrl();
+  ~CDVDSpyMJCtrl();
+
+DECLARE_REGISTRY_RESOURCEID(IDR_DVDSPYMJCTRL)
+
+DECLARE_PROTECT_FINAL_CONSTRUCT()
+
+BEGIN_COM_MAP(CDVDSpyMJCtrl)
+  COM_INTERFACE_ENTRY(IDVDSpyMJCtrl)
+  COM_INTERFACE_ENTRY(IDispatch)
+  COM_INTERFACE_ENTRY(IViewObjectEx)
+  COM_INTERFACE_ENTRY(IViewObject2)
+  COM_INTERFACE_ENTRY(IViewObject)
+  COM_INTERFACE_ENTRY(IOleInPlaceObjectWindowless)
+  COM_INTERFACE_ENTRY(IOleInPlaceObject)
+  COM_INTERFACE_ENTRY2(IOleWindow, IOleInPlaceObjectWindowless)
+  COM_INTERFACE_ENTRY(IOleInPlaceActiveObject)
+  COM_INTERFACE_ENTRY(IOleControl)
+  COM_INTERFACE_ENTRY(IOleObject)
+  COM_INTERFACE_ENTRY(IPersistStreamInit)
+  COM_INTERFACE_ENTRY2(IPersist, IPersistStreamInit)
+  COM_INTERFACE_ENTRY(ISpecifyPropertyPages)
+  COM_INTERFACE_ENTRY(IQuickActivate)
+  COM_INTERFACE_ENTRY(IPersistStorage)
+  COM_INTERFACE_ENTRY(IDataObject)
+  COM_INTERFACE_ENTRY(IProvideClassInfo)
+  COM_INTERFACE_ENTRY(IProvideClassInfo2)
+END_COM_MAP()
+
+BEGIN_PROP_MAP(CDVDSpyMJCtrl)
+  PROP_DATA_ENTRY("_cx", m_sizeExtent.cx, VT_UI4)
+  PROP_DATA_ENTRY("_cy", m_sizeExtent.cy, VT_UI4)
+  // Example entries
+  // PROP_ENTRY("Property Description", dispid, clsid)
+  // PROP_PAGE(CLSID_StockColorPage)
+END_PROP_MAP()
+
+BEGIN_MSG_MAP(CDVDSpyMJCtrl)
+  CHAIN_MSG_MAP(CComCompositeControl<CDVDSpyMJCtrl>)
+  COMMAND_HANDLER(IDC_RESET, BN_CLICKED, OnClickedReset)
+  MESSAGE_HANDLER(WM_TIMER, OnTimer)
+END_MSG_MAP()
+// Handler prototypes:
+//  LRESULT MessageHandler(UINT uMsg, WPARAM wParam, LPARAM lParam, BOOL& bHandled);
+//  LRESULT CommandHandler(WORD wNotifyCode, WORD wID, HWND hWndCtl, BOOL& bHandled);
+//  LRESULT NotifyHandler(int idCtrl, LPNMHDR pnmh, BOOL& bHandled);
+
+BEGIN_SINK_MAP(CDVDSpyMJCtrl)
+  //Make sure the Event Handlers have __stdcall calling convention
+END_SINK_MAP()
+
+  STDMETHOD(OnAmbientPropertyChange)(DISPID dispid)
+  {
+    if (dispid == DISPID_AMBIENT_BACKCOLOR)
+    {
+      SetBackgroundColorFromAmbient();
+      FireViewChange();
+    }
+    return IOleControlImpl<CDVDSpyMJCtrl>::OnAmbientPropertyChange(dispid);
+  }
+
+
+
+// IViewObjectEx
+  DECLARE_VIEW_STATUS(0)
+
+// IDVDSpyMJCtrl
+public:
+  STDMETHOD(Reset)();
+  STDMETHOD(get_RefreshInterval)(/*[out, retval]*/ short *pVal);
+  STDMETHOD(put_RefreshInterval)(/*[in]*/ short newVal);
+  STDMETHOD(Init)(/*[in]*/ LPDISPATCH pDisp);
+
+  enum { IDD = IDD_DVDSPYMJCTRL };
+
+protected:
+  LRESULT OnClickedReset(WORD wNotifyCode, WORD wID, HWND hWndCtl, BOOL& bHandled);
+  LRESULT OnTimer(UINT uMsg, WPARAM wParam, LPARAM lParam, BOOL& bHandled);
+
+  IMJAutomationPtr m_pMJ;
+  int m_nRefreshInterval;
+};
+
+#endif //__DVDSPYMJCTRL_H_
