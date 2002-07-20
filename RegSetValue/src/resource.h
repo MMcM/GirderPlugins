9d58ea122dea64f2e0e659d9719d471a1e74c6b2
Mike McMahon <mmcm@sourceforge.net>
2002-07-20 23:58:42 +0000

    Import release 1.4

--- RegSetValue/src/resource.h
+++ RegSetValue/src/resource.h
@@ -9,6 +9,9 @@
 #define IDC_KEY                         1002
 #define IDC_VALUEL                      1003
 #define IDC_VALUE                       1004
+#define IDC_SZ                          1005
+#define IDC_DWORD                       1006
+#define IDC_TYPEL                       1009
 
 // Next default values for new objects
 // 
@@ -17,7 +20,7 @@
 #define _APS_NO_MFC                     1
 #define _APS_NEXT_RESOURCE_VALUE        103
 #define _APS_NEXT_COMMAND_VALUE         40001
-#define _APS_NEXT_CONTROL_VALUE         1005
+#define _APS_NEXT_CONTROL_VALUE         1006
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
 #endif
