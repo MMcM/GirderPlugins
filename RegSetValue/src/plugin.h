0bee88653a8e30531ace15a37c94d6e32b88b89c
Mike McMahon <mmcm@sourceforge.net>
2002-08-11 04:15:28 +0000

    Import release 1.5

--- RegSetValue/src/plugin.h
+++ RegSetValue/src/plugin.h
@@ -1,34 +1,14 @@
-/***************************************************************************************/
-/*                                                                                     */
-/*  Girder 3.1 Plugin                                                                  */
-/*  User interface                                                                     */
-/*                                                                                     */
-/*  Copyright 2000 (c) Ron Bessems                                                     */
-/*  GNU General Public License                                                         */
-/*                                                                                     */
-/*                                                                                     */
-/***************************************************************************************/
+/* Global interfaces */
 
 #define PLUGINNUM 117
 #define PLUGINNAME "RegSetValue"
-#define PLUGINVERSION "1.4"
+#define PLUGINVERSION "1.5"
 
-#include <girder.h>
+extern HINSTANCE g_hInstance;
+extern s_functions SF;
 
-
-#ifdef GIRDER_CPP
-	p_command				CurCommand;
-   s_functions          SF;
-	HINSTANCE				hInstance;
-#endif
-
-#ifndef GIRDER_CPP
-	extern p_command			 CurCommand;		
-   extern s_functions       SF;
-	extern HINSTANCE	   	hInstance;
-#endif
-
-
-/* Commmon routines */
+extern void OpenCommandUI();
+extern void CloseCommandUI();
+extern void UpdateCommandUI(p_command command);
 
 extern PCHAR DecodeKeyValue(PCHAR value, PHKEY phkey);
--- RegSetValue/src/ui.cpp
+++ RegSetValue/src/ui.cpp
@@ -1,113 +1,106 @@
-/***************************************************************************************/
-/*                                                                                     */
-/*  Girder 3.1 Plugin                                                                  */
-/*  User interface                                                                     */
-/*                                                                                     */
-/*  Copyright 2000 (c) Ron Bessems                                                     */
-/*  GNU General Public License                                                         */
-/*                                                                                     */
-/*                                                                                     */
-/***************************************************************************************/
+/* User interface. */
 
-#include <windows.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include "resource.h"
+#include "stdafx.h"
 #include "plugin.h"
+#include "resource.h"
 
 /* Local variables */
-HWND hDialog;
-HANDLE ConfigThreadHandle;
+p_command g_pCommand = NULL;
+HWND g_hCommandDialog = NULL;
+HANDLE g_hCommandThread = NULL;
 
-static
-BOOL SaveUISettings(HWND hwnd)
+static BOOL SaveUISettings(HWND hwnd)
 {
   char buf[1024];
   
-  EnterCriticalSection(&CurCommand->critical_section);
+  EnterCriticalSection(&g_pCommand->critical_section);
   
   GetWindowText(GetDlgItem(hwnd, IDC_KEY), buf, sizeof(buf));
-  SF.realloc_pchar(&(CurCommand->svalue1), buf);
+  SF.realloc_pchar(&g_pCommand->svalue1, buf);
 
   GetWindowText(GetDlgItem(hwnd, IDC_VALUE), buf, sizeof(buf));
-  SF.realloc_pchar(&(CurCommand->svalue2), buf);
+  SF.realloc_pchar(&g_pCommand->svalue2, buf);
   
   if (SendMessage(GetDlgItem(hwnd, IDC_DWORD), BM_GETCHECK, 0, 0))
-    CurCommand->ivalue1 = REG_DWORD;
+    g_pCommand->ivalue1 = REG_DWORD;
   else
-    CurCommand->ivalue1 = REG_SZ;
+    g_pCommand->ivalue1 = REG_SZ;
 
-  CurCommand->actiontype=PLUGINNUM;
-  SF.set_command(CurCommand);
+  g_pCommand->actiontype=PLUGINNUM;
+  SF.set_command(g_pCommand);
 
   HKEY hkey;
-  PCHAR pval = DecodeKeyValue(CurCommand->svalue1, &hkey);
+  PCHAR pval = DecodeKeyValue(g_pCommand->svalue1, &hkey);
   if (NULL == pval)
   {
-    LeaveCriticalSection(&CurCommand->critical_section);
+    LeaveCriticalSection(&g_pCommand->critical_section);
     return FALSE;
   }
   LONG rc = RegQueryValueEx(hkey, pval, NULL, NULL, NULL, NULL);
   RegCloseKey(hkey);
   if (ERROR_SUCCESS != rc) {
-    LeaveCriticalSection(&CurCommand->critical_section);
-    sprintf(buf, "Value does not exist: %s", CurCommand->svalue1);
+    LeaveCriticalSection(&g_pCommand->critical_section);
+    sprintf(buf, "Value does not exist: %s", g_pCommand->svalue1);
     MessageBox(0, buf, "Error", MB_ICONERROR);
     return FALSE;
   }
   
-  LeaveCriticalSection(&CurCommand->critical_section);  
+  LeaveCriticalSection(&g_pCommand->critical_section);  
   EnableWindow(GetDlgItem(hwnd, IDC_APPLY), FALSE);
   return TRUE;
 }
      
-static
-void EmptyUI(HWND hwnd)
+static void LoadUISettings(HWND hwnd)
 {
-  SetWindowText(GetDlgItem(hwnd, IDC_KEY), "");
-  SetWindowText(GetDlgItem(hwnd, IDC_VALUE), "");
-}
-
-static
-void LoadUISettings(HWND hwnd)
-{
-  EnterCriticalSection(&CurCommand->critical_section);
-
-  if (!CurCommand->ivalue1) {
-    // Compatibility.
-    if (CurCommand->bvalue1) {
-      CurCommand->bvalue1 = FALSE;
-      char buf[32];
-      sprintf(buf, "%d", CurCommand->lvalue1);
-      CurCommand->lvalue1 = 0;
-      SF.realloc_pchar(&(CurCommand->svalue2), buf);
-      CurCommand->ivalue1 = REG_DWORD;
-    }
-    else
-      CurCommand->ivalue1 = REG_SZ;
+  if (NULL == g_pCommand) {
+    EnableWindow(GetDlgItem(hwnd, IDC_KEY), FALSE);
+    EnableWindow(GetDlgItem(hwnd, IDC_VALUE), FALSE);
+    EnableWindow(GetDlgItem(hwnd, IDC_APPLY), FALSE);
+    return;
   }
 
-  SetWindowText(GetDlgItem(hwnd, IDC_KEY), CurCommand->svalue1);
-  SetWindowText(GetDlgItem(hwnd, IDC_VALUE), CurCommand->svalue2);
-  SendMessage(GetDlgItem(hwnd, IDC_SZ), BM_SETCHECK, 
-              (CurCommand->ivalue1 == REG_SZ), 0);
-  SendMessage(GetDlgItem(hwnd, IDC_DWORD), BM_SETCHECK, 
-              (CurCommand->ivalue1 == REG_DWORD), 0);
-  
-  LeaveCriticalSection(&CurCommand->critical_section);
-}
+  EnterCriticalSection(&g_pCommand->critical_section);
 
-static
-BOOL CALLBACK DialogProc(  HWND hwnd,  UINT uMsg, WPARAM wParam, LPARAM lParam)
+  if (!g_pCommand->ivalue1) {
+    // Compatibility.
+    if (g_pCommand->bvalue1) {
+      g_pCommand->bvalue1 = FALSE;
+      char buf[32];
+      sprintf(buf, "%d", g_pCommand->lvalue1);
+      g_pCommand->lvalue1 = 0;
+      SF.realloc_pchar(&(g_pCommand->svalue2), buf);
+      g_pCommand->ivalue1 = REG_DWORD;
+    }
+    else
+      g_pCommand->ivalue1 = REG_SZ;
+  }
+
+  SetWindowText(GetDlgItem(hwnd, IDC_KEY), g_pCommand->svalue1);
+  SetWindowText(GetDlgItem(hwnd, IDC_VALUE), g_pCommand->svalue2);
+  SendMessage(GetDlgItem(hwnd, IDC_SZ), BM_SETCHECK, 
+              (g_pCommand->ivalue1 == REG_SZ), 0);
+  SendMessage(GetDlgItem(hwnd, IDC_DWORD), BM_SETCHECK, 
+              (g_pCommand->ivalue1 == REG_DWORD), 0);
+  
+  EnableWindow(GetDlgItem(hwnd, IDC_KEY), TRUE);
+  EnableWindow(GetDlgItem(hwnd, IDC_VALUE), TRUE);
+  EnableWindow(GetDlgItem(hwnd, IDC_APPLY), FALSE);
+
+  LeaveCriticalSection(&g_pCommand->critical_section);
+}
+     
+static BOOL CALLBACK CommandDialogProc(HWND hwnd,  UINT uMsg, 
+                                       WPARAM wParam, LPARAM lParam)
 {
   switch (uMsg) {
   case WM_INITDIALOG:
     {
       char trans[256];
       	
-      hDialog = hwnd;
+      g_hCommandDialog = hwnd;
 
-      SendMessage(hwnd, WM_SETICON, ICON_SMALL, (LPARAM)LoadIcon(hInstance, MAKEINTRESOURCE(PLUGINICON)));
+      SendMessage(hwnd, WM_SETICON, ICON_SMALL, 
+                  (LPARAM)LoadIcon(g_hInstance, MAKEINTRESOURCE(PLUGINICON)));
 
       SetWindowText(hwnd, PLUGINNAME);
 			
@@ -156,7 +149,7 @@ BOOL CALLBACK DialogProc(  HWND hwnd,  UINT uMsg, WPARAM wParam, LPARAM lParam)
 
     case IDC_KEY:
     case IDC_VALUE:
-      if ( HIWORD(wParam)==EN_CHANGE) {
+      if (HIWORD(wParam) == EN_CHANGE) {
         EnableWindow(GetDlgItem(hwnd, IDC_APPLY), TRUE);
       }
       break;
@@ -175,96 +168,45 @@ BOOL CALLBACK DialogProc(  HWND hwnd,  UINT uMsg, WPARAM wParam, LPARAM lParam)
 
   case WM_USER+100:
     LoadUISettings(hwnd);
-    EnableWindow(GetDlgItem(hwnd, IDC_APPLY), FALSE);
     return 1;
-
-  case WM_USER+101:
-    EmptyUI(hwnd);
-    return 1;
-
-  case WM_USER+102:
-    if (wParam==1) {
-      EnableWindow(GetDlgItem(hwnd, IDC_APPLY), FALSE);
-      EnableWindow(GetDlgItem(hwnd, IDOK), TRUE);
-      EnableWindow(GetDlgItem(hwnd, IDC_KEY), TRUE);
-      EnableWindow(GetDlgItem(hwnd, IDC_VALUE), TRUE);
-    }
-    else {
-      EnableWindow(GetDlgItem(hwnd, IDC_APPLY), FALSE);
-      EnableWindow(GetDlgItem(hwnd, IDOK), FALSE);
-      EnableWindow(GetDlgItem(hwnd, IDC_KEY), FALSE);
-      EnableWindow(GetDlgItem(hwnd, IDC_VALUE), FALSE);
-    }
-    return 1;			
   }
+
   return 0;
 }
 
-static DWORD WINAPI ConfigThread( LPVOID lpParameter )
+static DWORD WINAPI CommandThread(LPVOID lpParam)
 {
-  BOOL fResult;
-
-  fResult = DialogBoxParam(hInstance, MAKEINTRESOURCE(IDD_DIALOG), NULL, 
-                           DialogProc, (LPARAM)lpParameter);
-
-  hDialog=0;
-  
+  BOOL result = DialogBoxParam(g_hInstance, MAKEINTRESOURCE(IDD_DIALOG), NULL, 
+                               CommandDialogProc, (LPARAM)lpParam);
+  g_hCommandDialog = NULL;
   return 0;
 }
 
-void Update_Config()
-{
-
-  SendMessage(hDialog, WM_USER+100, 0,0);
-
-}
-
-void Empty_Config()
-{
-
-  SendMessage(hDialog, WM_USER+101, 0,0);
-
-}
-
-void Enable_Config(BOOL bValue)
-{
-  if (bValue)
-    SendMessage(hDialog, WM_USER+102, 1,0);
-  else
-    SendMessage(hDialog, WM_USER+102, 0,0);
-
-}
-
-void Show_Config()
+void OpenCommandUI()
 {
    DWORD dwThreadId;
-
-   
-   if (hDialog != 0)   
-   {
-      SetForegroundWindow(hDialog);
+   if (NULL != g_hCommandDialog) {
+     SetForegroundWindow(g_hCommandDialog);
    }
-   else
-   {
-      ConfigThreadHandle=CreateThread(NULL,0,&ConfigThread,NULL,0,&dwThreadId);
-      if (ConfigThreadHandle==0)
-         MessageBox(0, "Cannot create dialogthread.", "Error", MB_OK);
-    
+   else {
+     g_hCommandThread = CreateThread(NULL, 0, &CommandThread, NULL, 0, &dwThreadId);
+     if (NULL == g_hCommandThread)
+       MessageBox(NULL, "Cannot create dialog thread.", "Error", MB_OK);
    }
-
-
-   return ;
 }
 
-void Close_Config()
+void UpdateCommandUI(p_command command)
 {
-  
-  if ( hDialog!=NULL) {
+  g_pCommand = command;
+  if (NULL != g_hCommandDialog)
+    SendMessage(g_hCommandDialog, WM_USER+100, 0, 0);
+}
 
-    SendMessage(hDialog, WM_DESTROY,0,0);
-    WaitForSingleObject(ConfigThreadHandle,4000);
-    CloseHandle(ConfigThreadHandle);
-
+void CloseCommandUI()
+{
+  if (NULL != g_hCommandDialog) {
+    SendMessage(g_hCommandDialog, WM_DESTROY, 0, 0);
+    WaitForSingleObject(g_hCommandThread, 4000);
+    CloseHandle(g_hCommandThread);
   }
-  
 }
