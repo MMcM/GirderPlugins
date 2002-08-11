3ccbd669baee6dad28b65f2cfc5d7d6e4fe29ec9
Mike McMahon <mmcm@sourceforge.net>
2002-08-11 04:06:10 +0000

    Import release 1.3

---
+++ SetWaveDev/src/stdafx.h
@@ -0,0 +1,27 @@
+// stdafx.h : include file for standard system include files,
+//  or project specific include files that are used frequently, but
+//      are changed infrequently
+//
+
+#if !defined(AFX_STDAFX_H__C628136E_3849_4D68_B605_EBA9DE32DF55__INCLUDED_)
+#define AFX_STDAFX_H__C628136E_3849_4D68_B605_EBA9DE32DF55__INCLUDED_
+
+#if _MSC_VER > 1000
+#pragma once
+#endif // _MSC_VER > 1000
+
+#define WIN32_LEAN_AND_MEAN		// Exclude rarely-used stuff from Windows headers
+#include <windows.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <mmsystem.h>
+// From mmddk.h
+#define DRVM_MAPPER 0x2000
+
+#include <girder.h>
+
+//{{AFX_INSERT_LOCATION}}
+// Microsoft Visual C++ will insert additional declarations immediately before the previous line.
+
+#endif // !defined(AFX_STDAFX_H__C628136E_3849_4D68_B605_EBA9DE32DF55__INCLUDED_)
--- SetWaveDev/src/ui.cpp
+++ SetWaveDev/src/ui.cpp
@@ -1,50 +1,38 @@
-/***************************************************************************************/
-/*                                                                                     */
-/*  Girder 3.1 Plugin                                                                  */
-/*  User interface                                                                     */
-/*                                                                                     */
-/*  Copyright 2000 (c) Ron Bessems                                                     */
-/*  GNU General Public License                                                         */
-/*                                                                                     */
-/*                                                                                     */
-/***************************************************************************************/
+/* User interface. */
 
-#include <windows.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include "resource.h"
+#include "stdafx.h"
 #include "plugin.h"
+#include "resource.h"
 
 /* Local variables */
-HWND hDialog;
-HANDLE ConfigThreadHandle;
+p_command g_pCommand = NULL;
+HWND g_hCommandDialog = NULL;
+HANDLE g_hCommandThread = NULL;
 
-static
-BOOL SaveUISettings(HWND hwnd)
+static BOOL SaveUISettings(HWND hwnd)
 {
   char buf[1024];
 
-   EnterCriticalSection(&CurCommand->critical_section);
+  EnterCriticalSection(&g_pCommand->critical_section);
 
   if (SendMessage(GetDlgItem(hwnd, IDC_RECORD), BM_GETCHECK, 0, 0))
-    CurCommand->ivalue1 = 1;
+    g_pCommand->ivalue1 = 1;
   else
-    CurCommand->ivalue1 = 0;
+    g_pCommand->ivalue1 = 0;
   
   GetWindowText(GetDlgItem(hwnd, IDC_DEVICE), buf, sizeof(buf));
-  SF.realloc_pchar(&(CurCommand->svalue1), buf);
+  SF.realloc_pchar(&g_pCommand->svalue1, buf);
 
-  CurCommand->actiontype=PLUGINNUM;
-  SF.set_command(CurCommand);
+  g_pCommand->actiontype = PLUGINNUM;
+  SF.set_command(g_pCommand);
   
-  LeaveCriticalSection(&CurCommand->critical_section);
+  LeaveCriticalSection(&g_pCommand->critical_section);
   
   EnableWindow(GetDlgItem(hwnd, IDC_APPLY), FALSE);
   return TRUE;
 }
      
-static
-void LoadCombo(HWND hwnd)
+static void LoadCombo(HWND hwnd)
 {
   HWND combo = GetDlgItem(hwnd, IDC_DEVICE);
   SendMessage(combo, CB_RESETCONTENT, 0, 0);
@@ -56,39 +44,43 @@ void LoadCombo(HWND hwnd)
   }
 }
 
-static
-void EmptyUI(HWND hwnd)
+static void LoadUISettings(HWND hwnd)
 {
-  SendMessage(GetDlgItem(hwnd, IDC_PLAYBACK), BM_SETCHECK, TRUE, 0);
-  SendMessage(GetDlgItem(hwnd, IDC_RECORD), BM_SETCHECK, FALSE, 0);
+  if (NULL == g_pCommand) {
+    EnableWindow(GetDlgItem(hwnd, IDC_PLAYBACK), FALSE);
+    EnableWindow(GetDlgItem(hwnd, IDC_RECORD), FALSE);
+    EnableWindow(GetDlgItem(hwnd, IDC_DEVICE), FALSE);
+    EnableWindow(GetDlgItem(hwnd, IDC_APPLY), FALSE);
+    return;
+  }
+
+  EnterCriticalSection(&g_pCommand->critical_section);
+
+  SendMessage(GetDlgItem(hwnd, IDC_PLAYBACK), BM_SETCHECK, (g_pCommand->ivalue1 == 0), 0);
+  SendMessage(GetDlgItem(hwnd, IDC_RECORD), BM_SETCHECK, (g_pCommand->ivalue1 == 1), 0);
   LoadCombo(hwnd);
-  SendMessage(GetDlgItem(hwnd, IDC_DEVICE), CB_SETCURSEL, 0, 0);
+  SetWindowText(GetDlgItem(hwnd, IDC_DEVICE), g_pCommand->svalue1);
+
+  EnableWindow(GetDlgItem(hwnd, IDC_PLAYBACK), TRUE);
+  EnableWindow(GetDlgItem(hwnd, IDC_RECORD), TRUE);
+  EnableWindow(GetDlgItem(hwnd, IDC_DEVICE), TRUE);
+  EnableWindow(GetDlgItem(hwnd, IDC_APPLY), FALSE);
+
+  LeaveCriticalSection(&g_pCommand->critical_section);
 }
-
-static
-void LoadUISettings(HWND hwnd)
-{
-  EnterCriticalSection(&CurCommand->critical_section);
-
-  SendMessage(GetDlgItem(hwnd, IDC_PLAYBACK), BM_SETCHECK, (CurCommand->ivalue1 == 0), 0);
-  SendMessage(GetDlgItem(hwnd, IDC_RECORD), BM_SETCHECK, (CurCommand->ivalue1 == 1), 0);
-  LoadCombo(hwnd);
-  SetWindowText(GetDlgItem(hwnd, IDC_DEVICE), CurCommand->svalue1);
-
-  LeaveCriticalSection(&CurCommand->critical_section);
-}
-
-static
-BOOL CALLBACK DialogProc(  HWND hwnd,  UINT uMsg, WPARAM wParam, LPARAM lParam)
+     
+static BOOL CALLBACK CommandDialogProc(HWND hwnd,  UINT uMsg, 
+                                       WPARAM wParam, LPARAM lParam)
 {
   switch (uMsg) {
   case WM_INITDIALOG:
     {
       char trans[256];
       	
-      hDialog = hwnd;
+      g_hCommandDialog = hwnd;
 
-      SendMessage(hwnd, WM_SETICON, ICON_SMALL, (LPARAM)LoadIcon(hInstance, MAKEINTRESOURCE(PLUGINICON)));
+      SendMessage(hwnd, WM_SETICON, ICON_SMALL, 
+                  (LPARAM)LoadIcon(g_hInstance, MAKEINTRESOURCE(PLUGINICON)));
 
       SetWindowText(hwnd, PLUGINNAME);
 			
@@ -145,98 +137,45 @@ BOOL CALLBACK DialogProc(  HWND hwnd,  UINT uMsg, WPARAM wParam, LPARAM lParam)
 
   case WM_USER+100:
     LoadUISettings(hwnd);
-    EnableWindow(GetDlgItem(hwnd, IDC_APPLY), FALSE);
     return 1;
-
-  case WM_USER+101:
-    EmptyUI(hwnd);
-    return 1;
-
-  case WM_USER+102:
-    if (wParam==1) {
-      EnableWindow(GetDlgItem(hwnd, IDC_APPLY), FALSE);
-      EnableWindow(GetDlgItem(hwnd, IDOK), TRUE);
-      EnableWindow(GetDlgItem(hwnd, IDC_PLAYBACK), TRUE);
-      EnableWindow(GetDlgItem(hwnd, IDC_RECORD), TRUE);
-      EnableWindow(GetDlgItem(hwnd, IDC_DEVICE), TRUE);
-    }
-    else {
-      EnableWindow(GetDlgItem(hwnd, IDC_APPLY), FALSE);
-      EnableWindow(GetDlgItem(hwnd, IDOK), FALSE);
-      EnableWindow(GetDlgItem(hwnd, IDC_PLAYBACK), FALSE);
-      EnableWindow(GetDlgItem(hwnd, IDC_RECORD), FALSE);
-      EnableWindow(GetDlgItem(hwnd, IDC_DEVICE), FALSE);
-    }
-    return 1;			
   }
+
   return 0;
 }
 
-static DWORD WINAPI ConfigThread( LPVOID lpParameter )
+static DWORD WINAPI CommandThread(LPVOID lpParam)
 {
-  BOOL fResult;
-
-  fResult = DialogBoxParam(hInstance, MAKEINTRESOURCE(IDD_DIALOG), NULL, 
-                           DialogProc, (LPARAM)lpParameter);
-
-  hDialog=0;
-  
+  BOOL result = DialogBoxParam(g_hInstance, MAKEINTRESOURCE(IDD_DIALOG), NULL, 
+                               CommandDialogProc, (LPARAM)lpParam);
+  g_hCommandDialog = NULL;
   return 0;
 }
 
-void Update_Config()
-{
-
-  SendMessage(hDialog, WM_USER+100, 0,0);
-
-}
-
-void Empty_Config()
-{
-
-  SendMessage(hDialog, WM_USER+101, 0,0);
-
-}
-
-void Enable_Config(BOOL bValue)
-{
-  if (bValue)
-    SendMessage(hDialog, WM_USER+102, 1,0);
-  else
-    SendMessage(hDialog, WM_USER+102, 0,0);
-
-}
-
-void Show_Config()
+void OpenCommandUI()
 {
    DWORD dwThreadId;
-   // HANDLE ConfigThreadHandle;
-   
-   if (hDialog != 0)   
-   {
-      SetForegroundWindow(hDialog);
+   if (NULL != g_hCommandDialog) {
+     SetForegroundWindow(g_hCommandDialog);
    }
-   else
-   {
-      ConfigThreadHandle=CreateThread(NULL,0,&ConfigThread,NULL,0,&dwThreadId);
-      if (ConfigThreadHandle==0)
-         MessageBox(0, "Cannot create dialogthread.", "Error", MB_OK);
-    
+   else {
+     g_hCommandThread = CreateThread(NULL, 0, &CommandThread, NULL, 0, &dwThreadId);
+     if (NULL == g_hCommandThread)
+       MessageBox(NULL, "Cannot create dialog thread.", "Error", MB_OK);
    }
-
-
-   return ;
 }
 
-void Close_Config()
+void UpdateCommandUI(p_command command)
 {
-  
-  if ( hDialog!=NULL) {
+  g_pCommand = command;
+  if (NULL != g_hCommandDialog)
+    SendMessage(g_hCommandDialog, WM_USER+100, 0, 0);
+}
 
-    SendMessage(hDialog, WM_DESTROY,0,0);
-    WaitForSingleObject(ConfigThreadHandle,4000);
-    CloseHandle(ConfigThreadHandle);
-
+void CloseCommandUI()
+{
+  if (NULL != g_hCommandDialog) {
+    SendMessage(g_hCommandDialog, WM_DESTROY, 0, 0);
+    WaitForSingleObject(g_hCommandThread, 4000);
+    CloseHandle(g_hCommandThread);
   }
-  
 }
