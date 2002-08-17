acaf548e151d6825ba06447c78da75a4dab2cc7a
Mike McMahon <mmcm@sourceforge.net>
2002-08-17 17:55:12 +0000

    Import release 1.0

---
+++ Winamp3/src/resource.h
@@ -0,0 +1,28 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Developer Studio generated include file.
+// Used by ui.rc
+//
+#define PLUGINICON                      101
+#define IDD_COMMAND_DIALOG              102
+#define IDD_LEARN_DIALOG                103
+#define IDC_APPLY                       1000
+#define IDC_ACTIONL                     1001
+#define IDC_ACTION                      1002
+#define IDC_VALUEL                      1003
+#define IDC_VALUE                       1004
+#define IDC_RESULTL                     1005
+#define IDC_RESULT                      1006
+#define IDC_LEARN                       1007
+#define IDC_EVENTS                      1008
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NO_MFC                     1
+#define _APS_NEXT_RESOURCE_VALUE        104
+#define _APS_NEXT_COMMAND_VALUE         40000
+#define _APS_NEXT_CONTROL_VALUE         1009
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
