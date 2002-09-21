9641380dc4b6aabf7e58da40e5464ce1f8550006
Mike McMahon <mmcm@sourceforge.net>
2002-09-21 14:31:12 +0000

    Import release 2.0

--- LCD/src/resource.h
+++ LCD/src/resource.h
@@ -2,8 +2,9 @@
 // Microsoft Developer Studio generated include file.
 // Used by ui.rc
 //
-#define PLUGINICON                      101
-#define IDD_DIALOG                      102
+#define IDI_PLUGIN                      101
+#define IDD_CONFIG                      102
+#define IDD_ACTION                      103
 #define IDC_APPLY                       1000
 #define IDC_TYPEL                       1001
 #define IDC_TYPE                        1002
@@ -11,39 +12,56 @@
 #define IDC_VALSTR                      1004
 #define IDC_VALINT                      1005
 #define IDC_VAL_SPIN                    1006
-#define IDC_ROWL                        1007
-#define IDC_ROW                         1008
-#define IDC_ROW_SPIN                    1009
-#define IDC_USE_WRAP                    1010
-#define IDC_USE_COL                     1011
-#define IDC_COL                         1012
-#define IDC_COL_SPIN                    1013
-#define IDC_USE_REST                    1014
-#define IDC_USE_WIDTH                   1015
-#define IDC_WIDTH                       1016
-#define IDC_WIDTH_SPIN                  1017
-#define IDC_SCREENL                     1018
-#define IDC_ENABLE_LINE1                1019
-#define IDC_ENABLE_LINE2                1020
-#define IDC_ENABLE_LINE3                1021
-#define IDC_ENABLE_LINE4                1022
-#define IDC_LINE1                       1023
-#define IDC_LINE2                       1024
-#define IDC_LINE3                       1025
-#define IDC_LINE4                       1026
-#define IDC_MARQUEE_LINE1               1027
-#define IDC_MARQUEE_LINE2               1028
-#define IDC_MARQUEE_LINE3               1029
-#define IDC_MARQUEE_LINE4               1030
+#define IDC_VALBOOL                     1007
+#define IDC_ROWL                        1008
+#define IDC_ROW                         1009
+#define IDC_ROW_SPIN                    1010
+#define IDC_USE_WRAP                    1011
+#define IDC_USE_COL                     1012
+#define IDC_COL                         1013
+#define IDC_COL_SPIN                    1014
+#define IDC_USE_REST                    1015
+#define IDC_USE_WIDTH                   1016
+#define IDC_WIDTH                       1017
+#define IDC_WIDTH_SPIN                  1018
+#define IDC_SCREENL                     1019
+#define IDC_ENABLE_LINE1                1020
+#define IDC_ENABLE_LINE2                1021
+#define IDC_ENABLE_LINE3                1022
+#define IDC_ENABLE_LINE4                1023
+#define IDC_LINE1                       1024
+#define IDC_LINE2                       1025
+#define IDC_LINE3                       1026
+#define IDC_LINE4                       1027
+#define IDC_MARQUEE_LINE1               1028
+#define IDC_MARQUEE_LINE2               1029
+#define IDC_MARQUEE_LINE3               1030
+#define IDC_MARQUEE_LINE4               1031
+#define IDC_GPOL                        1032
+#define IDC_GPO                         1033
+#define IDC_GPO_SPIN                    1034
+#define IDC_DEVICEL                     1035
+#define IDC_DEVICE                      1036
+#define IDC_COLL                        1037
+#define IDC_PORTL                       1038
+#define IDC_PORT                        1039
+#define IDC_SPEEDL                      1040
+#define IDC_SPEED                       1041
+#define IDC_CONTRASTL                   1042
+#define IDC_CONTRAST                    1043
+#define IDC_BRIGHTNESS                  1044
+#define IDC_BRIGHTNESSL                 1045
+#define IDC_INPUT                       1046
+#define IDC_TEST                        1047
 
 // Next default values for new objects
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NO_MFC                     1
-#define _APS_NEXT_RESOURCE_VALUE        103
+#define _APS_NEXT_RESOURCE_VALUE        104
 #define _APS_NEXT_COMMAND_VALUE         40001
-#define _APS_NEXT_CONTROL_VALUE         1031
+#define _APS_NEXT_CONTROL_VALUE         1048
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
 #endif
