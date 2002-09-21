0ae1c11cd34ec391e9e5521aa0935a7ab0117eb7
Mike McMahon <mmcm@sourceforge.net>
2002-09-21 14:50:02 +0000

    Import release 2.0

--- LCD/src/plugin.h
+++ LCD/src/plugin.h
@@ -2,22 +2,32 @@
 
 #define PLUGINNUM 118
 #define PLUGINNAME "LCD"
-#define PLUGINVERSION "1.14"
+#define PLUGINVERSION "2.0"
 
 extern HINSTANCE g_hInstance;
 extern s_functions SF;
 
+extern void OpenConfigUI();
+extern void CloseConfigUI();
 extern void OpenCommandUI();
 extern void CloseCommandUI();
 extern void UpdateCommandUI(p_command command);
 
-extern int nDisplayCols, nDisplayRows;
-extern LPSTR pDisplayBuf;
+class DisplayDevice;
+
+extern int DisplayWidth();
+extern int DisplayHeight();
+extern int DisplayGPOs();
+extern void DisplayClose();
+extern void DisplayReopen(DisplayDevice *device);
+extern BOOL DisplayEnableInput();
+extern void DisplayDisableInput();
+extern void DisplaySendEvent(BYTE b);
 
 class DisplayCommandState;
 
 enum DisplayValueType {
-  valNONE, valSTR, valINT, valVAR, valSCREEN
+  valNONE, valSTR, valINT, valBOOL, valVAR
 };
 
 struct DisplayAction
@@ -37,7 +47,7 @@ extern void DisplayFilename(DisplayCommandState& state);
 extern void DisplayClose(DisplayCommandState& state);
 extern void DisplayClear(DisplayCommandState& state);
 extern void DisplayScreen(DisplayCommandState& state);
+extern void DisplayGPO(DisplayCommandState& state);
 
 extern void DisplayCommand(p_command command,
                            PCHAR status, int statuslen);
-extern void DisplayClose();
