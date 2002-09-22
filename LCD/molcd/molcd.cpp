1e083c033229222b1ccbe228531b60b83f159336
Mike McMahon <mmcm@sourceforge.net>
2002-09-22 05:13:44 +0000

    Import release 2.0

---
+++ LCD/molcd/molcd.cpp
@@ -0,0 +1,248 @@
+/* Matrix Orbital device implementation */
+
+#include "stdafx.h"
+
+HINSTANCE g_hInstance;
+
+// See http://www.matrixorbital.com/products.htm for list of
+// supported displays.
+
+struct DeviceEntry {
+  const char *devname;
+  int cols;
+  int rows;
+  BOOL vfd;
+  BOOL keypad;
+  int gpos;
+} DeviceEntries[] = {
+  { "LCD2021", 20, 2, FALSE, FALSE, 1 },
+  { "LCD4021", 40, 2, FALSE, FALSE, 1 },
+  { "LCD2041", 20, 4, FALSE, FALSE, 1 },
+  { "LCD4041", 40, 4, FALSE, FALSE, 1 },
+  { "LK162", 16, 2, FALSE, TRUE, 6 },
+  { "LK202", 20, 2, FALSE, TRUE, 6 },
+  { "LK204", 20, 4, FALSE, TRUE, 6 },
+  { "LK402", 40, 2, FALSE, TRUE, 7 },
+  { "LK404", 40, 4, FALSE, TRUE, 1 },
+  { "VFD2021", 20, 2, TRUE, FALSE, 1 },
+  { "VFD2041", 20, 4, TRUE, FALSE, 1 },
+  { "VK202", 20, 2, TRUE, TRUE, 6 },
+  { "VK204", 20, 4, TRUE, TRUE, 6 },
+};
+
+#define countof(x) sizeof(x)/sizeof(x[0])
+
+class MatrixOrbitalDisplay : public DisplayDevice
+{
+public:
+  MatrixOrbitalDisplay(HWND parent, LPCSTR devname);
+  ~MatrixOrbitalDisplay();
+  
+  virtual void DeviceDisplay(int row, int col, LPCSTR str, int length);
+  virtual BOOL DeviceOpen();
+  virtual void DeviceClose();
+  virtual void DeviceClear();
+  virtual BOOL DeviceHasContrast();
+  virtual BOOL DeviceHasBrightness();
+  virtual BOOL DeviceHasKeypad();
+  virtual BOOL DeviceEnableInput();
+  virtual void DeviceDisableInput();
+  virtual void DeviceInput(BYTE b);
+  virtual int DeviceGetGPOs();
+  virtual void DeviceSetGPO(int gpo, BOOL on);
+
+protected:
+  BOOL m_vfd, m_keypad;
+  int m_gpos;
+};
+
+MatrixOrbitalDisplay::MatrixOrbitalDisplay(HWND parent, LPCSTR devname)
+{
+  for (int i = 0; i < countof(DeviceEntries); i++) {
+    DeviceEntry *entry = DeviceEntries + i;
+    if (!strcmp(devname, entry->devname)) {
+      m_cols = entry->cols;
+      m_rows = entry->rows;
+      m_vfd = entry->vfd;
+      m_keypad = entry->keypad;
+      m_gpos = entry->gpos;
+      break;
+    }
+  }
+  m_portType = portSERIAL;
+  strcpy(m_port, "COM1");
+  m_portSpeed = CBR_19200;
+}
+
+MatrixOrbitalDisplay::~MatrixOrbitalDisplay()
+{
+}
+
+inline static BYTE contrast(int percent)
+{
+  return (percent * 0xFF) / 100;
+}
+
+inline static BYTE brightness(int percent)
+{
+  if (percent <= 25)
+    return 0;
+  else if (percent <= 50)
+    return 1;
+  else if (percent <= 75)
+    return 2;
+  else
+    return 3;
+}
+
+BOOL MatrixOrbitalDisplay::DeviceOpen()
+{
+  if (!OpenSerial(m_enableInput))
+    return FALSE;
+
+  BYTE buf[128];
+  int nb = 0;
+  buf[nb++] = 0xFE;
+  buf[nb++] = 0x42;             // Display on ...
+  buf[nb++] = 0x00;             // ... permanently
+  buf[nb++] = 0xFE;
+  buf[nb++] = 0x44;             // Auto line wrap off
+  buf[nb++] = 0xFE;
+  buf[nb++] = 0x52;             // Auto scroll off
+  buf[nb++] = 0xFE;
+  buf[nb++] = 0x4B;             // Underline cursor off
+  buf[nb++] = 0xFE;
+  buf[nb++] = 0x54;             // Block cursor off
+  if (m_vfd) {
+    buf[nb++] = 0xFE;
+    buf[nb++] = 0x59;           // Set brightness ...
+    buf[nb++] = brightness(m_brightness);
+  }
+  else {
+    buf[nb++] = 0xFE;
+    buf[nb++] = 0x50;           // Set contrast ...
+    buf[nb++] = contrast(m_contrast);
+  }
+  buf[nb++] = 0xFE;
+  buf[nb++] = 0x58;             // Clear display
+  WriteSerial(buf, nb);
+
+  return TRUE;
+}
+
+void MatrixOrbitalDisplay::DeviceClose()
+{
+  BYTE buf[128];
+  int nb = 0;
+  buf[nb++] = 0xFE;
+  buf[nb++] = 0x58;             // Clear display
+  buf[nb++] = 0xFE;
+  buf[nb++] = 0x46;             // Display off
+  WriteSerial(buf, nb);
+
+  CloseSerial();
+}
+
+void MatrixOrbitalDisplay::DeviceClear()
+{
+  BYTE buf[128];
+  int nb = 0;
+  buf[nb++] = 0xFE;
+  buf[nb++] = 0x58;             // Clear display
+  WriteSerial(buf, nb);
+}
+
+void MatrixOrbitalDisplay::DeviceDisplay(int row, int col, LPCSTR str, int length)
+{
+  BYTE buf[128];
+  int nb = 0;
+  if ((row == 0) && (col == 0)) {
+    buf[nb++] = 0xFE;
+    buf[nb++] = 0x48;           // Send cursor home
+  }
+  else {
+    buf[nb++] = 0xFE;
+    buf[nb++] = 0x47;           // Set cursor position ...
+    buf[nb++] = col + 1;
+    buf[nb++] = row + 1;
+  }
+  for (int i = 0; i < length; i++) {
+    char ch = str[i];
+    // TODO: Translate character set?
+    buf[nb++] = ch;
+  }
+  WriteSerial(buf, nb);
+}
+
+BOOL MatrixOrbitalDisplay::DeviceHasContrast()
+{
+  return !m_vfd;
+}
+
+BOOL MatrixOrbitalDisplay::DeviceHasBrightness()
+{
+  return m_vfd;
+}
+
+BOOL MatrixOrbitalDisplay::DeviceHasKeypad()
+{
+  return m_keypad;
+}
+
+BOOL MatrixOrbitalDisplay::DeviceEnableInput()
+{
+  if (!Open()) return FALSE;    // Get a port handle.
+  return EnableSerialInput();
+}
+
+void MatrixOrbitalDisplay::DeviceDisableInput()
+{
+  DisableSerialInput();
+}
+
+void MatrixOrbitalDisplay::DeviceInput(BYTE b)
+{
+  char buf[2];
+  buf[0] = b;
+  buf[1] = '\0';
+  DisplaySendEvent(buf);
+}
+
+int MatrixOrbitalDisplay::DeviceGetGPOs()
+{
+  return m_gpos;
+}
+
+void MatrixOrbitalDisplay::DeviceSetGPO(int gpo, BOOL on)
+{
+  BYTE buf[128];
+  int nb = 0;
+  buf[nb++] = 0xFE;
+  buf[nb++] = (on) ? 0x57 : 0x56; // General purpose output on/off ...
+  if (m_gpos > 0)
+    buf[nb++] = gpo;
+  WriteSerial(buf, nb);
+}
+
+extern "C" __declspec(dllexport)
+DisplayDevice *CreateDisplayDevice(HWND parent, LPCSTR name)
+{
+  return new MatrixOrbitalDisplay(parent, name);
+}
+
+/* Called by windows */
+BOOL WINAPI DllMain(HANDLE hModule, DWORD dwReason,  LPVOID lpReserved)
+{
+  switch(dwReason) {
+  case DLL_PROCESS_ATTACH:
+    g_hInstance = (HINSTANCE)hModule;
+    break;
+  case DLL_THREAD_ATTACH:
+    break;
+  case DLL_THREAD_DETACH:
+    break;
+  case DLL_PROCESS_DETACH:
+    break;
+  }
+  return TRUE;
+}
---
+++ LCD/src/display.h
@@ -0,0 +1,176 @@
+/* Display device interface */
+
+#ifdef LCD_EXPORTS
+#define LCD_API __declspec(dllexport)
+#else
+#define LCD_API __declspec(dllimport)
+#endif
+
+class LCD_API DisplayDevice
+{
+public:
+  static HKEY GetSettingsKey();
+  static BOOL GetSettingString(HKEY hkey, LPCSTR valkey,
+                               LPSTR value, size_t vallen);
+  static BOOL GetSettingInt(HKEY hkey, LPCSTR valkey, int& value);
+  static BOOL GetSettingBool(HKEY hkey, LPCSTR valkey, BOOL& defval);
+  static void SetSettingString(HKEY hkey, LPCSTR valkey, LPCSTR value);
+  static void SetSettingInt(HKEY hkey, LPCSTR valkey, int value);
+  static void SetSettingBool(HKEY hkey, LPCSTR valkey, BOOL value);
+
+  // Create device based on registry settings.
+  static DisplayDevice *Create(HWND parent = NULL, LPCSTR lib = NULL, LPCSTR dev = NULL);
+  virtual ~DisplayDevice();
+
+  BOOL IsOpen() {
+    return m_open;
+  }
+  BOOL Open();
+  void Close();
+
+  int GetWidth() {
+    return m_cols;
+  }
+  int GetHeight() {
+    return m_rows;
+  }
+  BOOL HasSetSize() {
+    return DeviceHasSetSize();
+  }
+  void SetSize(int width, int height);
+
+  enum PortType_t { portNONE, portSERIAL, portPARALLEL };
+  PortType_t GetPortType() {
+    return m_portType;
+  }
+  LPCSTR GetPort() {
+    return m_port;
+  }
+  void SetPort(LPCSTR port) {
+    strncpy(m_port, port, sizeof(m_port));
+  }
+  int GetPortSpeed() {
+    return m_portSpeed;
+  }
+  void SetPortSpeed(int speed) {
+    m_portSpeed = speed;
+  }
+
+  BOOL HasContrast() {
+    return DeviceHasContrast();
+  }
+  int GetContrast() {
+    return m_contrast;
+  }
+  void SetContrast(int percent) {
+    m_contrast = percent;
+  }
+
+  BOOL HasBrightness() {
+    return DeviceHasBrightness();
+  }
+  BOOL HasBacklight() {
+    return DeviceHasBacklight();
+  }
+  int GetBrightness() {
+    return m_brightness;
+  }
+  void SetBrightness(int percent) {
+    m_brightness = percent;
+  }
+
+  BOOL HasKeypad() {
+    return DeviceHasKeypad();
+  }
+  BOOL GetEnableInput() {
+    return m_enableInput;
+  }
+  void SetEnableInput(BOOL enable) {
+    m_enableInput = enable;
+  }
+  BOOL EnableInput() {
+    return DeviceEnableInput();
+  }
+  void DisableInput() {
+    DeviceDisableInput();
+  }
+
+  int GetGPOs() {
+    return DeviceGetGPOs();
+  }
+  void SetGPO(int gpo, BOOL on) {
+    DeviceSetGPO(gpo, on);
+  }
+
+  void LoadSettings(HKEY hkey);
+  void SaveSettings(HKEY hkey);
+
+  // Drawing.
+  void Display(int row, int col, int width, LPCSTR str);
+  void Clear();
+  void Test();
+
+protected:
+  typedef DisplayDevice *(*CreateFun_t)(HWND parent, LPCSTR devname);
+  DisplayDevice();
+
+  // Override these for each device.
+  virtual void DeviceDisplay(int row, int col, LPCSTR str, int length) = 0;
+  virtual BOOL DeviceOpen();
+  virtual void DeviceClose();
+  virtual void DeviceClear();
+  virtual void DeviceSetMarquee();
+  virtual void DeviceClearMarquee();
+  virtual BOOL DeviceHasSetSize();
+  virtual BOOL DeviceHasContrast();
+  virtual BOOL DeviceHasBrightness();
+  virtual BOOL DeviceHasBacklight();
+  virtual BOOL DeviceHasKeypad();
+  virtual BOOL DeviceEnableInput();
+  virtual void DeviceDisableInput();
+  virtual void DeviceInput(BYTE b);
+  virtual int DeviceGetGPOs();
+  virtual void DeviceSetGPO(int gpo, BOOL on);
+  virtual void DeviceLoadSettings(HKEY hkey);
+  virtual void DeviceSaveSettings(HKEY hkey);
+
+  // These can be used by device implementations.
+  void DisplayInternal(int row, int col, LPCSTR str, int length);
+  void SetSimulatedMarquee();
+  void ClearSimulatedMarquee();
+  void StepSimulatedMarquee();
+  friend void WINAPI MarqueeTimer(HWND hwnd, UINT uMsg, UINT idEvent, DWORD dwTime);
+
+  BOOL OpenSerial(BOOL asynch = FALSE);
+  BOOL WriteSerial(LPBYTE data, size_t len);
+  BOOL EnableSerialInput();
+  void DisableSerialInput();
+  void CloseSerial();
+  friend DWORD WINAPI SerialInputThread(LPVOID lpParam);
+
+  // State.
+  BOOL m_open;
+  int m_cols, m_rows;
+  LPSTR m_buffer;
+
+  LPSTR m_marquee;
+  int m_marqueeRow, m_marqueeLen, m_marqueePos;
+  int m_marqueePixelWidth, m_marqueeSpeed;
+  UINT m_marqueeTimer;
+
+  PortType_t m_portType;
+  char m_port[8];
+  int m_portSpeed;
+  BOOL m_portRTS, m_portDTR;
+  HANDLE m_portHandle;
+
+  int m_contrast, m_brightness;
+
+  BOOL m_enableInput;
+  HANDLE m_inputThread, m_inputStopEvent, m_outputEvent;
+};
+
+extern "C" {
+LCD_API void DisplayWin32Error(HWND parent, DWORD dwErr);
+LCD_API void DisplaySendEvent(LPCSTR event);
+}
