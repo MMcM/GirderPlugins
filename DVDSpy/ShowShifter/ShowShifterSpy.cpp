801c7519038ae8fc928bdd55521a7a2c0056b9e2
Mike McMahon <mmcm@sourceforge.net>
2002-11-10 18:39:44 +0000

    Import release 1.47

--- DVDSpy/ShowShifter/ShowShifterSpy.cpp
+++ DVDSpy/ShowShifter/ShowShifterSpy.cpp
@@ -1,10 +1,5 @@
 // ShowShifterSpy.cpp : Implementation of DLL Exports.
 
-
-// Note: Proxy/Stub Information
-//      To build a separate proxy/stub DLL, 
-//      run nmake -f ShowShifterSpyps.mk in the project directory.
-
 #include "stdafx.h"
 #include "resource.h"
 #include <initguid.h>
@@ -28,18 +23,16 @@ END_OBJECT_MAP()
 extern "C"
 BOOL WINAPI DllMain(HINSTANCE hInstance, DWORD dwReason, LPVOID /*lpReserved*/)
 {
-    if (dwReason == DLL_PROCESS_ATTACH)
-    {
-         ATLTRACE(_T("ShowShifterSpy loading\n")) ;
-        _Module.Init(ObjectMap, hInstance, &LIBID_ShowShifterSpyLib);
-        DisableThreadLibraryCalls(hInstance);
-    }
-    else if (dwReason == DLL_PROCESS_DETACH)
-    {
-         ATLTRACE(_T("ShowShifterSpy unloading\n")) ;
-        _Module.Term();
-    }
-    return TRUE;    // ok
+  if (dwReason == DLL_PROCESS_ATTACH) {
+    ATLTRACE(_T("ShowShifterSpy loading\n")) ;
+    _Module.Init(ObjectMap, hInstance, &LIBID_ShowShifterSpyLib);
+    DisableThreadLibraryCalls(hInstance);
+  }
+  else if (dwReason == DLL_PROCESS_DETACH) {
+    ATLTRACE(_T("ShowShifterSpy unloading\n")) ;
+    _Module.Term();
+  }
+  return TRUE;    // ok
 }
 
 /////////////////////////////////////////////////////////////////////////////
@@ -47,7 +40,7 @@ BOOL WINAPI DllMain(HINSTANCE hInstance, DWORD dwReason, LPVOID /*lpReserved*/)
 
 STDAPI DllCanUnloadNow(void)
 {
-    return (_Module.GetLockCount()==0) ? S_OK : S_FALSE;
+  return (_Module.GetLockCount()==0) ? S_OK : S_FALSE;
 }
 
 /////////////////////////////////////////////////////////////////////////////
@@ -55,7 +48,7 @@ STDAPI DllCanUnloadNow(void)
 
 STDAPI DllGetClassObject(REFCLSID rclsid, REFIID riid, LPVOID* ppv)
 {
-    return _Module.GetClassObject(rclsid, riid, ppv);
+  return _Module.GetClassObject(rclsid, riid, ppv);
 }
 
 /////////////////////////////////////////////////////////////////////////////
@@ -63,8 +56,8 @@ STDAPI DllGetClassObject(REFCLSID rclsid, REFIID riid, LPVOID* ppv)
 
 STDAPI DllRegisterServer(void)
 {
-    // registers object, typelib and all interfaces in typelib
-    return _Module.RegisterServer(TRUE);
+  // registers object, typelib and all interfaces in typelib
+  return _Module.RegisterServer(TRUE);
 }
 
 /////////////////////////////////////////////////////////////////////////////
@@ -72,7 +65,5 @@ STDAPI DllRegisterServer(void)
 
 STDAPI DllUnregisterServer(void)
 {
-    return _Module.UnregisterServer(TRUE);
+  return _Module.UnregisterServer(TRUE);
 }
-
-
--- DVDSpy/WMP/WMPSpy.cpp
+++ DVDSpy/WMP/WMPSpy.cpp
@@ -1,10 +1,5 @@
 // WMPSpy.cpp : Implementation of DLL Exports.
 
-
-// Note: Proxy/Stub Information
-//      To build a separate proxy/stub DLL, 
-//      run nmake -f WMPSpyps.mk in the project directory.
-
 #include "stdafx.h"
 #include "resource.h"
 #include <initguid.h>
@@ -12,12 +7,13 @@
 
 #include "WMPSpy_i.c"
 #include "SpyCtrl.h"
-
+#include "SpyPlugin.h"
 
 CComModule _Module;
 
 BEGIN_OBJECT_MAP(ObjectMap)
 OBJECT_ENTRY(CLSID_SpyCtrl, CSpyCtrl)
+OBJECT_ENTRY(CLSID_SpyPlugin, CSpyPlugin)
 END_OBJECT_MAP()
 
 /////////////////////////////////////////////////////////////////////////////
@@ -26,14 +22,16 @@ END_OBJECT_MAP()
 extern "C"
 BOOL WINAPI DllMain(HINSTANCE hInstance, DWORD dwReason, LPVOID /*lpReserved*/)
 {
-    if (dwReason == DLL_PROCESS_ATTACH)
-    {
-        _Module.Init(ObjectMap, hInstance, &LIBID_WMPSPYLib);
-        DisableThreadLibraryCalls(hInstance);
-    }
-    else if (dwReason == DLL_PROCESS_DETACH)
-        _Module.Term();
-    return TRUE;    // ok
+  if (dwReason == DLL_PROCESS_ATTACH) {
+    ATLTRACE(_T("WMPSpy loading\n")) ;
+    _Module.Init(ObjectMap, hInstance, &LIBID_WMPSPYLib);
+    DisableThreadLibraryCalls(hInstance);
+  }
+  else if (dwReason == DLL_PROCESS_DETACH) {
+    ATLTRACE(_T("WMPSpy unloading\n")) ;
+    _Module.Term();
+  }
+  return TRUE;    // ok
 }
 
 /////////////////////////////////////////////////////////////////////////////
@@ -41,7 +39,7 @@ BOOL WINAPI DllMain(HINSTANCE hInstance, DWORD dwReason, LPVOID /*lpReserved*/)
 
 STDAPI DllCanUnloadNow(void)
 {
-    return (_Module.GetLockCount()==0) ? S_OK : S_FALSE;
+  return (_Module.GetLockCount()==0) ? S_OK : S_FALSE;
 }
 
 /////////////////////////////////////////////////////////////////////////////
@@ -49,7 +47,7 @@ STDAPI DllCanUnloadNow(void)
 
 STDAPI DllGetClassObject(REFCLSID rclsid, REFIID riid, LPVOID* ppv)
 {
-    return _Module.GetClassObject(rclsid, riid, ppv);
+  return _Module.GetClassObject(rclsid, riid, ppv);
 }
 
 /////////////////////////////////////////////////////////////////////////////
@@ -57,8 +55,8 @@ STDAPI DllGetClassObject(REFCLSID rclsid, REFIID riid, LPVOID* ppv)
 
 STDAPI DllRegisterServer(void)
 {
-    // registers object, typelib and all interfaces in typelib
-    return _Module.RegisterServer(TRUE);
+  // registers object, typelib and all interfaces in typelib
+  return _Module.RegisterServer(TRUE);
 }
 
 /////////////////////////////////////////////////////////////////////////////
@@ -66,7 +64,5 @@ STDAPI DllRegisterServer(void)
 
 STDAPI DllUnregisterServer(void)
 {
-    return _Module.UnregisterServer(TRUE);
+  return _Module.UnregisterServer(TRUE);
 }
-
-
