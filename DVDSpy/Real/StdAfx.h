65c51ff19e7f24e2360d27b1a3f88847b4fe7b18
Mike McMahon <mmcm@sourceforge.net>
2002-12-15 22:04:38 +0000

    Import release 1.50

---
+++ DVDSpy/Real/StdAfx.h
@@ -0,0 +1,31 @@
+// stdafx.h : include file for standard system include files,
+//  or project specific include files that are used frequently, but
+//      are changed infrequently
+//
+
+#if !defined(AFX_STDAFX_H__4058151E_1BC9_410F_BCCF_259A4850949C__INCLUDED_)
+#define AFX_STDAFX_H__4058151E_1BC9_410F_BCCF_259A4850949C__INCLUDED_
+
+#if _MSC_VER > 1000
+#pragma once
+#endif // _MSC_VER > 1000
+
+#include <pntypes.h>
+#include <pncom.h>
+#include <rmaplugn.h>
+#include <rmacore.h>
+#include <rmapsink.h>
+#include <rmaclsnk.h>
+#include <rmamon.h>
+#include <rmacomm.h>
+#include <rmapckts.h>
+
+#define WIN32_LEAN_AND_MEAN
+#include <windows.h>
+
+#include <stdio.h>
+
+//{{AFX_INSERT_LOCATION}}
+// Microsoft Visual C++ will insert additional declarations immediately before the previous line.
+
+#endif // !defined(AFX_STDAFX_H__4058151E_1BC9_410F_BCCF_259A4850949C__INCLUDED_)
---
+++ DVDSpy/Real/initguid.cpp
@@ -0,0 +1,12 @@
+// Local copies of GUID values.
+
+// NB: No stdafx
+#define INITGUID
+#include <pntypes.h>
+#include <pncom.h>
+#include <rmaplugn.h>
+#include <rmacore.h>
+#include <rmapsink.h>
+#include <rmaclsnk.h>
+#include <rmamon.h>
+#include <rmacomm.h>
