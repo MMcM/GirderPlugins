31ccc906646492e87b6afd3161e81daf859894c6
Mike McMahon <mmcm@sourceforge.net>
2003-01-18 01:26:52 +0000

    Clarify the intent of the .lua files.

--- DVDSpy/WeatherSettings.lua
+++ DVDSpy/WeatherSettings.lua
@@ -1,6 +1,7 @@
 -- $Header$
 
--- Location(s) to get weather data for (can use ZIP codes for USA).
+-- Location(s) to get weather data for (can use ZIP codes for USA; elsewhere
+-- see http://www.msnbc.com/news/wea_front.asp?ta=y&tab=BW&tp=&ctry=&cp1=1).
 -- (Some tweaking of the GML samples will be required if more than one
 -- entry is added, to keep the displays straight.)
 AddWeatherLocation("NLXX0002")  -- Amsterdam
