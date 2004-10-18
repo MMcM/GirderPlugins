94a4b636db26f40b0ef92e38a68d0ad7ab610a3c
Mike McMahon <mmcm@sourceforge.net>
2004-10-18 00:56:02 +0000

    Version correction.

--- DVDSpy/foobar2000/foo_girder_dvdspy.cpp
+++ DVDSpy/foobar2000/foo_girder_dvdspy.cpp
@@ -7,7 +7,7 @@ $Header$
 
 DECLARE_COMPONENT_VERSION(
   "Foobar2000 / Girder DVDSpy component",
-  "0.1",
+  "1.63",
   "Spy on Foobar state/track info to pass to Girder DVDSpy")
 
 // Title Format string for Title event
