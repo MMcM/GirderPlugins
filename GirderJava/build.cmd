0a56df8daa1ebf65950f4e099e7ec375706eb018
cvs2git <admin@example.com>
2005-03-27 19:01:56 +0000

    This commit was manufactured by cvs2git to create branch 'GIRDER_4'.

--- GirderJava/build.cmd
+++
@@ -1,8 +0,0 @@
-@rem $Header$
-cd java
-javac -g nl\girder\api\*.java
-jar -cf ..\..\..\GirderJava.jar nl\girder\api\*.class
-javah -d ..\jni nl.girder.api.GirderCommand nl.girder.api.GirderLink nl.girder.api.GirderSupport
-javadoc -sourcepath . -d ..\javadocs nl.girder.api
-cd ..
-nmake /f "GirderJava.mak" CFG="GirderJava - Win32 Release"
--- SetWaveDev/src/stdafx.cpp
+++
@@ -1,6 +0,0 @@
-// stdafx.cpp : source file that includes just the standard includes
-// $Header$
-//  SetWaveDev.pch will be the pre-compiled header
-//  stdafx.obj will contain the pre-compiled type information
-
-#include "stdafx.h"
