cd3cce199925b9924c712f53bad2f372728d8bf5
Mike McMahon <mmcm@sourceforge.net>
2006-11-09 02:40:51 +0000

    Use Girder registry function.

--- DVDSpy/unreadmail.lua
+++ DVDSpy/unreadmail.lua
@@ -11,115 +11,9 @@ print(unreadmail:FromOutlook(true))
 
 --]]
 
-
-if not mail then mail = {} end
-
 unreadmail = {
 
-  -- Get the information as used by SHGetUnreadMailCount.
-  HKCU = 2147483649,
   UnreadMailRoot = "Software\\Microsoft\\Windows\\CurrentVersion\\UnreadMail",
-  FromRegistry = function(self, allAccounts)
-    local loc, svc, reg
-    -- WMI is the only way to get at the registry with key enumeration from automation.
-    -- Girder's win package has single value read / write.
-    -- Likewise WshShell.
-    loc = luacom.CreateObject("WbemScripting.SWbemLocator")
-    svc = loc:ConnectServer("", "root\\default")
-    reg = svc:Get("StdRegProv")
-
-    local error, ignore, keys
-    error, ignore, ignore, keys = reg:EnumKey(self.HKCU, self.UnreadMailRoot, nil)
-    if error ~= 0 then return end
-
-    local function getval(key)
-      local error, ignore, ignore, ignore, value = 
-        reg:GetDWORDValue(self.HKCU, self.UnreadMailRoot .. "\\" .. key, "MessageCount",
-                          nil)
-      if error == 0 then
-        local result = { UnRead = value }
-        setmetatable(result, self.UnReadEntry)
-        return result
-      end
-    end
-
-    if allAccounts then
-      local result = {}
-      for i = 1, table.getn(keys) do
-        key = keys[i]
-        result[key] = getval(key)
-      end
-      setmetatable(result, self.UnReadList)
-      return result
-    else
-      return getval(keys[1])
-    end
-  end,
-
-  -- Get unread mail count from outlook, optionally including amount of
-  -- spam removed and counts from other stores with folders named
-  -- "Inbox".  This is useful when an inbox rule moves incoming mail
-  -- from some account(s) to another store.  Outlook will no longer
-  -- report that as unread to the shell.
-  FromOutlook = function(self, includeJunk, includeOtherStores)
-    -- GetObject or nil
-    local function ProtectedGetObject(...)
-      local ok, obj = pcall(luacom.GetObject, unpack(arg))
-      if ok then return obj end
-    end
-
-    -- items(index) or nil
-    local function ProtectedGetItem(...)
-      local obj = arg[1]
-      local ok, item = pcall(obj.Item, unpack(arg))
-      if ok then return item end
-    end
-
-    local outlook, session, defaultInbox, junkBox, result
-
-    -- Obtain connection each time, in case application has been closed
-    -- and reopened.
-    outlook = ProtectedGetObject("Outlook.Application")
-    if not outlook then return end
-
-    session = outlook.Session
-    defaultInbox = session:GetDefaultFolder(6) -- olFolderInbox
-
-    result = { UnRead = defaultInbox.UnReadItemCount }
-    setmetatable(result, self.UnReadEntry)
-
-    junkBox = session:getDefaultFolder(23) -- olFolderJunk
-    if includeJunk then
-      result.Junk = junkBox.Items.Count
-    end
-
-    if includeOtherStores then
-      result = { Default = result }
-      setmetatable(result, self.UnReadList)
-
-      local enum, store, otherBox, otherResult
-
-      enum = luacom.GetEnumerator(session.Folders)
-      store = enum:Next()
-      while store do
-        if store.StoreID ~= defaultInbox.StoreID then
-          otherBox = ProtectedGetItem(store.Folders, defaultInbox.Name)
-          if otherBox then
-            otherResult = { UnRead = otherBox.UnReadItemCount }
-            setmetatable(otherResult, self.UnReadEntry)
-            result[store.Name] = otherResult
-            otherBox = ProtectedGetItem(store.Folders, junkBox.Name)
-            if otherBox and includeJunk then
-              otherResult.Junk = otherBox.Items.Count
-            end
-          end
-        end
-        store = enum:Next()
-      end
-    end
-    
-    return result
-  end,
 
   -- Use metatables to make values returned format nicely for display
   -- on LCD automatically.
@@ -161,3 +55,140 @@ unreadmail = {
   }
 
 }
+
+-- Get the information as used by SHGetUnreadMailCount.
+if win then
+
+-- Use Girder's registry object.
+function unreadmail.FromRegistry(self, allAccounts)
+  local reg = win.CreateRegistry("HKCU", self.UnreadMailRoot)
+  local keys = reg:ListKeys()
+
+  local function getval(key)
+    local reg = win.CreateRegistry("HKCU", self.UnreadMailRoot .. "\\" .. key)
+    local result = { UnRead = reg:Read("MessageCount") }
+    setmetatable(result, self.UnReadEntry)
+    return result
+  end
+
+  if allAccounts then
+    local result = {}
+    for i = 1, table.getn(keys) do
+      key = keys[i]
+      result[key] = getval(key)
+    end
+    setmetatable(result, self.UnReadList)
+    return result
+  else
+    return getval(keys[1])
+  end
+end
+
+else
+
+-- The following works even outside Girder.
+unreadmail.HKCU = 2147483649
+
+function unreadmail.FromRegistry(self, allAccounts)
+  local loc, svc, reg
+  -- WMI is the only way to get at the registry with key enumeration from automation.
+  -- Girder's win package has single value read / write.
+  -- Likewise WshShell.
+  loc = luacom.CreateObject("WbemScripting.SWbemLocator")
+  svc = loc:ConnectServer("", "root\\default")
+  reg = svc:Get("StdRegProv")
+
+  local error, ignore, keys
+  error, ignore, ignore, keys = reg:EnumKey(self.HKCU, self.UnreadMailRoot, nil)
+  if error ~= 0 then return end
+
+  local function getval(key)
+    local error, ignore, ignore, ignore, value = 
+      reg:GetDWORDValue(self.HKCU, self.UnreadMailRoot .. "\\" .. key, "MessageCount",
+                        nil)
+    if error == 0 then
+      local result = { UnRead = value }
+      setmetatable(result, self.UnReadEntry)
+      return result
+    end
+  end
+
+  if allAccounts then
+    local result = {}
+    for i = 1, table.getn(keys) do
+      key = keys[i]
+      result[key] = getval(key)
+    end
+    setmetatable(result, self.UnReadList)
+    return result
+  else
+    return getval(keys[1])
+  end
+end
+
+end
+
+-- Get unread mail count from outlook, optionally including amount of
+-- spam removed and counts from other stores with folders named
+-- "Inbox".  This is useful when an inbox rule moves incoming mail
+-- from some account(s) to another store.  Outlook will no longer
+-- report that as unread to the shell.
+function unreadmail.FromOutlook(self, includeJunk, includeOtherStores)
+  -- GetObject or nil
+  local function ProtectedGetObject(...)
+    local ok, obj = pcall(luacom.GetObject, unpack(arg))
+    if ok then return obj end
+  end
+
+  -- items(index) or nil
+  local function ProtectedGetItem(...)
+    local obj = arg[1]
+    local ok, item = pcall(obj.Item, unpack(arg))
+    if ok then return item end
+  end
+
+  local outlook, session, defaultInbox, junkBox, result
+
+  -- Obtain connection each time, in case application has been closed
+  -- and reopened.
+  outlook = ProtectedGetObject("Outlook.Application")
+  if not outlook then return end
+
+  session = outlook.Session
+  defaultInbox = session:GetDefaultFolder(6) -- olFolderInbox
+
+  result = { UnRead = defaultInbox.UnReadItemCount }
+  setmetatable(result, self.UnReadEntry)
+
+  junkBox = session:getDefaultFolder(23) -- olFolderJunk
+  if includeJunk then
+    result.Junk = junkBox.Items.Count
+  end
+
+  if includeOtherStores then
+    result = { Default = result }
+    setmetatable(result, self.UnReadList)
+
+    local enum, store, otherBox, otherResult
+
+    enum = luacom.GetEnumerator(session.Folders)
+    store = enum:Next()
+    while store do
+      if store.StoreID ~= defaultInbox.StoreID then
+        otherBox = ProtectedGetItem(store.Folders, defaultInbox.Name)
+        if otherBox then
+          otherResult = { UnRead = otherBox.UnReadItemCount }
+          setmetatable(otherResult, self.UnReadEntry)
+          result[store.Name] = otherResult
+          otherBox = ProtectedGetItem(store.Folders, junkBox.Name)
+          if otherBox and includeJunk then
+            otherResult.Junk = otherBox.Items.Count
+          end
+        end
+      end
+      store = enum:Next()
+    end
+  end
+  
+  return result
+end
